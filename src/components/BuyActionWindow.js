93f18eeb4bd49352b7c84da7f577ab84404be0f1
Maddy398 <157033626+Maddy398@users.noreply.github.com>
2025-05-22 15:40:06 +0530

    Update BuyActionWindow.js

--- src/components/BuyActionWindow.js
+++ src/components/BuyActionWindow.js
@@ -12,7 +12,7 @@ const BuyActionWindow = ({ uid }) => {
   const [stockPrice, setStockPrice] = useState(0.0);
 
   const handleBuyClick = () => {
-    axios.post("https://zerodha-backend-0zxr.onrender.com/", {
+    axios.post("https://zerodha-backend-0zxr.onrender.com/newOrder", {
       name: uid,
       qty: stockQuantity,
       price: stockPrice,
