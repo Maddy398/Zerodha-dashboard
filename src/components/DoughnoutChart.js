0349b25157beaf5ac234707e618cb27c94c987b7
Maddy398 <157033626+Maddy398@users.noreply.github.com>
2025-05-23 17:06:14 +0530

    Update DoughnoutChart.js

--- src/components/DoughnoutChart.js
+++ src/components/DoughnoutChart.js
@@ -1,9 +1,64 @@
-import React from "react";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+// DoughnutChart.js
 import { Doughnut } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  Title,
+} from "chart.js";
+import ChartDataLabels from "chartjs-plugin-datalabels";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(ArcElement, Tooltip, Legend, Title, ChartDataLabels);
 
-export function DoughnutChart({ data }) {
-  return <Doughnut data={data} />;
-}
+export const DoughnutChart = ({ data }) => {
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "right",
+        labels: {
+          color: "#333",
+          font: {
+            size: 14,
+            weight: "bold",
+          },
+        },
+      },
+      tooltip: {
+        callbacks: {
+          label: function (tooltipItem) {
+            return `${tooltipItem.label}: ₹${tooltipItem.raw.toFixed(2)}`;
+          },
+        },
+      },
+      datalabels: {
+        color: "#fff",
+        font: {
+          weight: "bold",
+          size: 12,
+        },
+        formatter: (value, ctx) => {
+          let sum = ctx.chart._metasets[0].total;
+          let percentage = ((value / sum) * 100).toFixed(1) + "%";
+          return percentage;
+        },
+      },
+      title: {
+        display: true,
+        text: "Watchlist Stock Prices",
+        font: {
+          size: 18,
+        },
+        color: "#222",
+        padding: {
+          top: 10,
+          bottom: 30,
+        },
+      },
+    },
+    cutout: "65%", // for nice donut appearance
+  };
+
+  return <Doughnut data={data} options={options} />;
+};
